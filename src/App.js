33eff23ea21c352ea1ecb9fa74177fec9dbab0dc
Amira Mohammed <am201121999@gmail.com>
2025-05-29 10:53:04 +0300

    add small btn

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/homePage';
+import Drugs from './pages/drugPage';
+
 // import Login from './pages/loginPage';
 // import Register from './pages/registerPage';
 // import Dashboard from './pages/dashboardPage';
-import Drugs from './pages/drugPage'
+// import Drugs from './pages/drugPage'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} exact/>
+        <Route path="/" element={<Home/>}/>
         {/* <Route path="/login" element={Login} exact/> */}
         {/* <Route path="/register" element={Register} exact/> */}
         {/* <Route path="/dashboard" element={Dashboard} exact/> */}
-        <Route path="/Drugs" element={<Drugs />} exact/>
+        <Route path="/Drugs" element={<Drugs />} />
       </Routes>
     </Router>
   );
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,7 +1,12 @@
+import SmallBtn from "../components/shared/smallbtn";
+
 function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <SmallBtn name="Click Me"
+  btnColor="primary"
+  onClick={() => alert('Button clicked!')}/>
       <p>This is the main page of our application.</p>
     </div>
   );
