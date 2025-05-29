63464fb9ed786a2321be78e7b3bddf8151bd55c4
saramahmoud7689 <saramahmoud7689@gmail.com>
2025-05-29 08:12:21 +0300

    still error

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={Home} exact/>
+        <Route path="/" element={<Home />} exact/>
         {/* <Route path="/login" element={Login} exact/> */}
         {/* <Route path="/register" element={Register} exact/> */}
         {/* <Route path="/dashboard" element={Dashboard} exact/> */}
-        <Route path="/Drugs" element={Drugs} exact/>
+        <Route path="/Drugs" element={<Drugs />} exact/>
       </Routes>
     </Router>
   );
