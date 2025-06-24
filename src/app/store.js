e7b76e44163782c0aca81dd4781a6c74132fb57b
saramahmoud7689 <saramahmoud7689@gmail.com>
2025-06-24 22:33:27 +0300

    solve conflict

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import pharmacyReducer from '../features/pharmacyslice'; 
-<<<<<<< HEAD
-import cartReducer from "../features/cartSlice"
-=======
 import cartReducer from "../features/cart/cartSlice"
 import notificationReducer from "../features/notificationSlice"
->>>>>>> 124d9c8 (Notification Done)
 export const store = configureStore({
   reducer: {
     auth: authReducer,
