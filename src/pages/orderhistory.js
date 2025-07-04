e81142c926442e6c1a5fdcffe34505b8fdb5b1a7
saramahmoud7689 <saramahmoud7689@gmail.com>
2025-07-04 21:07:18 +0300

    retrive order history old design

--- src/pages/orderhistory.js
+++ src/pages/orderhistory.js
@@ -11,7 +11,7 @@ import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 
 const MySwal = withReactContent(Swal);
-
+//[SARA]: going back
 const OrderHistory = () => {
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
